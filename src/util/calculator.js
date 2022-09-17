1413a03b7bb8547ddadcd03f2e040a1ce96a76c0
Jung Young Jun <dudwns0921@gmail.com>
2022-09-17 19:24:42 +0900

    feat: 소수점 입력 로직 추가

--- src/util/calculator.js
+++ src/util/calculator.js
@@ -2,8 +2,34 @@ const MAX_LEN = 12;
 
 class Calculator {
   #units = [];
+  #currentNum = [];
+
+  #isLastUnitSign() {
+    return /[+ㅡ×÷%.]/.test(this.#units[this.#units.length - 1]);
+  }
 
   insert(str) {
+    if (/[+ㅡ×÷%.]/.test(str)) {
+      // 입력된 글자가 기호인지 판단
+      if (/[+ㅡ×÷%]/.test(str)) {
+        this.#currentNum = [];
+        // 연산 기호일 경우 현재 숫자를 초기화
+      } else if (str === '.') {
+        if (this.#currentNum.includes('.')) return;
+        if (this.#currentNum.length === 0) {
+          str = '0' + str;
+          for (const letter of str) {
+            this.#currentNum.push(letter);
+          }
+          this.#units.push(str);
+        } else {
+          this.#currentNum.push(str);
+        }
+      }
+      if (this.#isLastUnitSign() || this.#units.length === 0) return;
+    } else {
+      this.#currentNum.push(str);
+    }
     this.#units.push(str);
   }
 
@@ -15,12 +41,12 @@ class Calculator {
     this.#units = [];
   }
 
-  calculate() {
-    console.log('calculate units');
-  }
-
   reverse() {
     console.log('reverse unit');
+  }
+
+  calculate() {
+    console.log('calculate units');
   }
 
   getFormula() {
