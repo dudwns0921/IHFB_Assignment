75c0ce1f563a8407665f9827da9c5e2e2acfbb04
Jung Young Jun <dudwns0921@gmail.com>
2022-09-18 18:48:54 +0900

    feat: 계산기 메서드 로직 이슈 수정
    - delete 메서드 이슈 수정
    - clear 메서드 이슈 수정
    - insert 메서드 이슈 수정
    소수점 이후 기호 입력 불가 이슈 수정
    reverse 적용 이후 숫자 입력 이슈 수정
    reverse 적용 이후 소수점 입력 이슈 수정

--- src/util/calculator.js
+++ src/util/calculator.js
@@ -26,7 +26,12 @@ class Calculator {
   insert(str) {
     if (/[+ㅡ×÷%.]/.test(str)) {
       if (/[+ㅡ×÷%]/.test(str)) {
+        if (this.#lastUnit() === '.') {
+          this.#units.pop();
+          this.#currentNum.pop();
+        }
         this.#currentNum = [];
+        this.#isReverse = false;
       } else if (str === '.') {
         if (this.#currentNum.includes('.')) return;
         if (this.#currentNum.length === 0) {
@@ -38,11 +43,28 @@ class Calculator {
         } else {
           this.#currentNum.push(str);
         }
+        if (this.#isReverse) {
+          this.#units.pop();
+          this.#currentNum.pop();
+          this.#units.push(str, ')');
+          this.#currentNum.push(str, ')');
+          return;
+        }
       }
       if (/[+ㅡ×÷%.]/.test(this.#lastUnit()) || this.#units.length === 0) return;
     } else {
       if (this.#currentNum.length === MAX_LEN) return;
-      if (this.#lastCurrentNum() === '0' && this.#currentNum.length === 1) return;
+      if (this.#lastCurrentNum() === '0' && this.#currentNum.length === 1) {
+        this.#units.pop();
+        this.#currentNum.pop();
+      }
+      if (this.#isReverse) {
+        this.#units.pop();
+        this.#currentNum.pop();
+        this.#units.push(str, ')');
+        this.#currentNum.push(str, ')');
+        return;
+      }
       this.#currentNum.push(str);
     }
     this.#units.push(str);
@@ -50,19 +72,21 @@ class Calculator {
 
   delete() {
     this.#units.pop();
+    this.#currentNum.pop;
   }
 
   clear() {
     this.#units = [];
+    this.#currentNum = [];
   }
 
   reverse() {
-    this.#isReverse = !this.#isReverse;
     if (this.#currentNum.length === 0) return;
+    this.#isReverse = !this.#isReverse;
     if (this.#units.length === this.#currentNum.length) {
       this.#units = [];
       this.#reverseCurrentNum();
-      this.#units = this.#currentNum;
+      this.#units = JSON.parse(JSON.stringify(this.#currentNum));
     } else {
       let idx = -1;
       while (!/[+ㅡ×÷%]/.test(this.#units.at(idx))) {
@@ -74,7 +98,6 @@ class Calculator {
         this.#units.push(letter);
       }
     }
-    // 부호가 있을 때
   }
 
   calculate() {
